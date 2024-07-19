54aa5ba4e88ab161d184a25b0cc7e6b024fc33ac
Achmat Fauzi <achmatfauzi86@gmail.com>
2024-07-19 15:02:13 +0700

    Update socket.ts

--- src/components/socket.ts
+++ src/components/socket.ts
@@ -1,3 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io('https://test-nuxt-socketio.netlify.app/');
+export const socket = io('https://test-nuxt-socketio.netlify.app/', {
+    transports: ['websocket']
+  }
+);
